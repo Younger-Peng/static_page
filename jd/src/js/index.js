c7e667aa2426f7cfe1d5597cc0a8fe2395e79292
plearner <953413537@qq.com>
2017-05-26 00:13:45 +0800

    调整顶部搜索栏和header的css样式

--- jd/src/js/index.js
+++ jd/src/js/index.js
@@ -1,8 +1,18 @@
 (function(){
   let pageWidth = document.documentElement.clientWidth;
   let style = document.createElement('style')
-  style.innerHTML = 'html{font-size:' + pageWidth/16 + 'px;}'
+  window.rem = pageWidth/16
+  style.innerHTML = 'html{font-size:' + rem + 'px;}'
   document.head.appendChild(style)
+  //给顶部搜索栏添加滚动时背景色渐变函数
+
+  window.addEventListener('scroll', function(e){
+    let scrollTop = document.body.scrollTop
+    let n = scrollTop / (5.25*rem)
+    if(n>0.9) n = 0.9;
+    document.querySelector('.top').style.background = 'rgba(255,0,0,'+ n +')'
+  })
 })()
+
 
 
